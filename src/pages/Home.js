7be6f420446658efa310cdcbea58dd93b2c8ea70
EugenWay <relax211@gmail.com>
2021-07-19 14:27:16 +0100

    event listener functionality

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,7 +13,7 @@ import { hexToU8a, isHex } from "@polkadot/util";
 export const Home = () => {
   const [blocks, setBlocks] = useState([]);
   const [lastBlock, setLastBlock] = useState(null);
-  const [spec, setSpec] = useState(null)
+  const [spec, setSpec] = useState(null);
   const [alert, setAlert] = useState({
     isActive: false,
     type: "alert-primary",
@@ -27,13 +27,37 @@ export const Home = () => {
         const api = await ApiPromise.create({ provider: wsProvider });
         await api.isReady;
 
-        setSpec(api.runtimeVersion.specVersion.toString())
+        setSpec(api.runtimeVersion.specVersion.toString());
+
+        await api.query.system.events((events) => {
+
+          const newEvents = events.map((record) => {
+            // Extract the phase, event and the event types
+            const { event, phase } = record;
+            const types = event.typeDef;
+
+            const data = event.data.forEach((data, index) => {
+              console.log(`\t\t\t${types[index].type}: ${data.toString()}`);
+            });
+
+            return {
+              section: `${event.section}`,
+              method: `${event.method}`,
+              phase: `${phase}`,
+              data: event.data
+            }
+
+          }).filter((event) => event.section !== 'system' && (!['balances', 'treasury'].includes(event.section) || !['Deposit'].includes(event.method)) &&
+          (!['parasInclusion', 'inclusion'].includes(event.eventsection) || !['CandidateBacked', 'CandidateIncluded'].includes(event.method)))
+
+          console.log(newEvents)
+          
+        });
 
         // Subscribe to the new headers
         const unsubHeads = await api.derive.chain.subscribeNewHeads(
           async (lastHeader) => {
-
-            const info = await api.derive.accounts.info(`${lastHeader.author}`)
+            const info = await api.derive.accounts.info(`${lastHeader.author}`);
 
             setLastBlock(`${lastHeader.number}`);
             setBlocks((blocks) => [
@@ -42,14 +66,11 @@ export const Home = () => {
                 number: `${lastHeader.number}`,
                 hash: `${lastHeader.hash}`,
                 author: `${lastHeader.author}`,
-                identity: info.identity
+                identity: info.identity,
               },
             ]);
-
           }
         );
-        
-
       } catch (err) {
         console.log(err);
       }
@@ -57,13 +78,6 @@ export const Home = () => {
 
     connectChain();
   }, []);
-
-
-  useEffect(() => {
-    console.log(blocks)
-  },[blocks])
-
-
 
   const isValidAddressPolkadotAddress = (address) => {
     try {
@@ -100,7 +114,7 @@ export const Home = () => {
 
   return lastBlock ? (
     <div>
-      <BlockChainInfo lastBlock={lastBlock} runtimeVer={spec}/>
+      <BlockChainInfo lastBlock={lastBlock} runtimeVer={spec} />
       <Alert type={alert.type} msg={alert.msg} isActive={alert.isActive} />
       <Search handler={getDataSearch} />
       <BlockList list={blocks} />
